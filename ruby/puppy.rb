8a2e9b08bf3e0081b13ffc0a3d8a1ee811a11805
Bernardo Saravia <bernardo.saravia@yale.edu>
2016-06-28 06:14:07 -0400

    6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Cute puppy
+
+# CHARACTERISTICS ----------------
+# Size: small
+# Friendly: yes
+# Eye count: 2
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Jump
+# Hug
+# Fetch
+
+# puppy class experimentation on irb
