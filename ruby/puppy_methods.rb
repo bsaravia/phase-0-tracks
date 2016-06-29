9a82f713ead4cec1b90aa8d20d81cd842252261b
Bernardo Saravia <bernardo.saravia@yale.edu>
2016-06-29 16:22:32 -0400

    6.2 pairing

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(num)
+  	puts "Woof!\n" * num
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(num)
+  	num * 7
+  end
+
+  def jumps_on(thing)
+  	puts "The dog jumped on the #{thing}."
+  end
+
 end
 
+brock = Puppy.new
+brock.fetch("ball")
+brock.speak(5)
+brock.roll_over
+puts brock.dog_years(5)
+brock.jumps_on("table")
